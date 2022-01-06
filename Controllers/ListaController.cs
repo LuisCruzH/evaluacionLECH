78372cf4ed9b46fbf9befa5d3440a1f0983ad64a
luisCH <luiscruzcasi@gmail.com>
2022-01-06 01:31:56 -0600

    modal

--- Controllers/ListaController.cs
+++ Controllers/ListaController.cs
@@ -9,9 +9,24 @@ namespace evaluacionLECH.Controllers
 {
     public class ListaController : Controller
     {
+        #region ViewBag
+        List<SelectListItem> listEstatus = new List<SelectListItem>()
+            {
+                new SelectListItem {
+                    Text = "Enviado", Value = "1"
+                },
+                new SelectListItem {
+                    Text = "Autorizado", Value = "2"
+                },
+                new SelectListItem {
+                    Text = "Rechazado", Value = "3"
+                }
+            };
+        #endregion
         // GET: Lista
         public ActionResult Index()
         {
+            ViewBag.listEstatus = listEstatus;
             List<CapturaCLS> listaCaptura = null;
             using (var bd = new evaluacionBDEntities())
             {
@@ -27,6 +42,29 @@ namespace evaluacionLECH.Controllers
                                 ).ToList();
             }
             return View(listaCaptura);
+        }
+        public string editarlo(CapturaCLS oCapturaCLS, int titulo)
+        {
+            string rpta = "";
+            using (var bd = new evaluacionBDEntities())
+            {
+                prospecto oProspecto = bd.prospecto.Where(p => p.id_prospecto.Equals(titulo)).First();
+                oProspecto.estatus = oCapturaCLS.estatusProspecto;
+                oProspecto.observaciones = oCapturaCLS.observacionesProspecto;
+                rpta = bd.SaveChanges().ToString();
+            }
+            return rpta;
+        }
+        public JsonResult recuperarDatos (int titulo)
+        {
+            CapturaCLS oCapturaCLS = new CapturaCLS();
+            using (var bd = new evaluacionBDEntities())
+            {
+                prospecto oProspecto = bd.prospecto.Where(p => p.id_prospecto.Equals(titulo)).First();
+                oCapturaCLS.estatusProspecto = oProspecto.estatus.Value;
+                oCapturaCLS.observacionesProspecto = oProspecto.observaciones;
+            }
+            return Json(oCapturaCLS, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- Models/CapturaCLS.cs
+++ Models/CapturaCLS.cs
@@ -11,29 +11,37 @@ namespace evaluacionLECH.Models
         /*Prospecto*/
         [Display(Name ="ID")]
         public int iidProspecto { get; set; }
+        [Required]
         [Display(Name = "Nombre")]
         public string nombreProspecto { get; set; }
+        [Required]
         [Display(Name = "Apellido Paterno")]
         public string aPaternoProspecto { get; set; }
         [Display(Name = "Apellido Materno")]
         public string aMaternoProspecto { get; set; }
+        [Required]
         [Display(Name = "RFC")]
         public string rfcProspecto { get; set; }
+        [Required]
         [Display(Name = "Telefono")]
         public string telefonoProspecto { get; set; }
         [Display (Name ="Estatus")]
         public int estatusProspecto { get; set; }
         [Display(Name ="Observaciones")]
         public string observacionesProspecto { get; set; }
-        
+
 
         /*Datos de Prospecto*/
+        [Required]
         [Display(Name = "Calle")]
         public string calle { get; set; }
+        [Required]
         [Display(Name = "Numero")]
         public string numero { get; set; }
+        [Required]
         [Display(Name = "Colonia")]
         public string colonia { get; set; }
+        [Required]
         [Display(Name = "Codigo Postal")]
         public int codigoPostal { get; set; }
 
